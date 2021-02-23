972597c0f59c3507898757cc7d012593ad8559f3
vagrant <vagrant@localhost.localdomain>
2021-02-23 08:10:56 +0000

    create routts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+	belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :favorited_users, through: :likes, source: :user
+	has_many :comments, dependent: :destroy
+
 end
