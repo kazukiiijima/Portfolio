da2b6cb658dab481e050f0f6b66de1b366cef65c
vagrant <vagrant@localhost.localdomain>
2021-04-19 12:04:57 +0000

    add user img

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
   has_many :comments, dependent: :destroy
+  attachment :image
   def already_favorited?(post)
   	self.favorites.exists?(post_id: post.id)
   end
