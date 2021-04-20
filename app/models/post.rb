0a84d8c97dd6c88f078fe34adb756cfb2ab92a82
vagrant <vagrant@localhost.localdomain>
2021-04-20 13:03:54 +0000

    fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,6 @@ class Post < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	attachment :image
 
+	validates :title,  presence: { message: '動画タイトルが空欄です' }
+	validates :body, presence: { message: '「ここがオススメ！」が空欄です' }
 end
